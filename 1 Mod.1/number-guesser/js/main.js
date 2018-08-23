2fa6a8b51d191f4581470e8b1e7ac36b4387eb8e
chris023 <chris.meyer023@gmail.com>
2018-08-23 14:13:21 -0600

    Refactored to match airbnb styleguide

--- 1 Mod.1/number-guesser/js/main.js
+++ 1 Mod.1/number-guesser/js/main.js
@@ -1,203 +1,194 @@
 // Buttons
-const guessButton           = document.querySelector("#guessButton");
-const clearButton           = document.querySelector("#clearButton");
-const resetButton           = document.querySelector("#resetButton");
-const settingsButton        = document.querySelector("#settingsIco");
-const helpButton            = document.querySelector("#helpIco");
+const guessButton = document.querySelector('#guessButton');
+const clearButton = document.querySelector('#clearButton');
+const resetButton = document.querySelector('#resetButton');
+const settingsButton = document.querySelector('#settingsIco');
+const helpButton = document.querySelector('#helpIco');
 
-//dynamic display windows
-const helpDialog            = document.querySelector("#helpDialog");
-const exitHelp              = document.querySelector("#closeHelp");
-const settingsWindow        = document.querySelector("#settingsWindow");
-const gameWindow            = document.querySelector("#gameWindow");
+// Dynamic display windows
+const helpDialog = document.querySelector('#helpDialog');
+const exitHelp = document.querySelector('#closeHelp');
+const settingsWindow = document.querySelector('#settingsWindow');
 
-//Input Field   
-const formInput             = document.querySelector("#guessValue");
+// Input field
+const formInput = document.querySelector('#guessValue');
 
-//Settings dialog hooks
-const lowValField           = document.querySelector("#lowVal");
-const highValField          = document.querySelector("#highVal");
-const exitSettings          = document.querySelector("#closeSettings");
+// Settings dialog hooks
+const lowValField = document.querySelector('#lowVal');
+const highValField = document.querySelector('#highVal');
+const exitSettings = document.querySelector('#closeSettings');
 
-//Displays game result on screen
-let gameResult              = document.querySelector("#gameResult");
+// Displays game result on screen
+const gameResult = document.querySelector('#gameResult');
 
-//Game level
-const currentLevelViewer    = document.querySelector("#currentLevel");
-let currentLevel            = 1;
+// Game level
+const currentLevelViewer = document.querySelector('#currentLevel');
+let currentLevel = 1;
 
-//Possible results to display
-let results = {
-    default: "Make a guess!",
-    correct: "BOOM!",
-    tooHigh: "That is too high",
-    tooLow: "That is too low"
-}
+// Possible results to display
+const results = {
+  default: 'Make a guess!',
+  correct: 'BOOM!',
+  tooHigh: 'That is too high',
+  tooLow: 'That is too low',
+};
 
-//Displays most recent guess
-let lastGuess = document.querySelector("#lastGuess");
+// Displays most recent guess
+const lastGuess = document.querySelector('#lastGuess');
 
-//Sets game's high and low values to guess between
+// Sets game's high and low values to guess between
 let lowVal = 0;
 let highVal = 100;
 
-//Sets the value we are trying to guess randomly
+function generateTargetVal() {
+  return Math.round(Math.random() * (highVal - lowVal)) + lowVal;
+}
+
 let targetVal = generateTargetVal();
 
+function validInput(userValue) {
+  const isValid = (lowVal <= userValue && userValue <= highVal);
+  if (isValid) {
+    formInput.setCustomValidity('');
+  } else {
+    formInput.setCustomValidity('Invalid Input');
+  }
 
-//Guess Button Handler
-guessButton.addEventListener("click", function(){
-    
-    let guessVal = parseInt(document.querySelector("#guessValue").value);
+  return isValid;
+}
 
-    if( !validInput(guessVal) ) {
-            
-        return;
-    }  
-    else{
+function adjustLevel(adjuster) {
+  currentLevel += adjuster;
+  highVal += (adjuster * 10);
+  highValField.value = highVal;
+  currentLevelViewer.innerText = currentLevel;
+}
 
-        lastGuess.innerHTML = guessVal;
+function levelUp() {
+  gameResult.innerHTML = results.correct;
+  adjustLevel(1);
+  targetVal = generateTargetVal();
+}
 
-        if      (guessVal ===   targetVal)     levelUp();
-        else if (guessVal >     targetVal)     gameResult.innerHTML = results.tooHigh;
-        else if (guessVal <     targetVal)     gameResult.innerHTML = results.tooLow;
-    }
+guessButton.addEventListener('click', () => {
+  const guessVal = parseInt(document.querySelector('#guessValue').value, 2);
 
+  if (!validInput(guessVal)) {
+    return;
+  }
+  lastGuess.innerHTML = guessVal;
 
-    //Handles reset button state
-    if(gameResult.innerText === results.default)
-        resetButton.disabled = true;    
-    else {     
-        resetButton.disabled = false;
-    }
-        
+  if (guessVal === targetVal) {
+    levelUp();
+  } else if (guessVal > targetVal) {
+    gameResult.innerHTML = results.tooHigh;
+  } else if (guessVal < targetVal) {
+    gameResult.innerHTML = results.tooLow;
+  }
+
+  // Handles reset button state
+  if (gameResult.innerText === results.default) {
+    resetButton.disabled = true;
+  } else {
+    resetButton.disabled = false;
+  }
 });
 
-
-//Handles the low value field in the settings dialog
-lowValField.addEventListener("keyup", function(){
-
-    if(validValFields()){
-        
-        clearValFieldsValidity();
-    
-        if(lowVal !== parseInt(lowValField.value)){
-            lowVal = parseInt(lowValField.value);
-            resetGame();
-        }
-    } else{
-        //highlights box red if invalid
-        lowValField.setCustomValidity("Invalid Input");
-    }
-});
-
-//Handles the high value field in the settings dialog
-highValField.addEventListener("keyup", function(){
-    
-    if(validValFields()){
-
-        clearValFieldsValidity();
-        
-        if(highVal !== parseInt(highValField.value)){
-            highVal = parseInt(highValField.value);
-            resetGame();
-        }
-    } else{
-        //highlights box red if invalid
-        highValField.setCustomValidity("Invalid Input");
-    }
-});
-
-//Clear Button click Handler
-clearButton.addEventListener("click", function(){
-    formInput.value = "";
-    disableClearGuess();
-});
-
-//Reset Button Click Handler
-resetButton.addEventListener("click", function()    {   resetGame();                    });
-
-//Clear, Guess Button state handler
-formInput.addEventListener("keyup", function()      {   disableClearGuess();            });
-
-//Help and Exit Button handlers
-helpButton.addEventListener("click", function()     {   toggleWindow(helpDialog);       });
-exitHelp.addEventListener("click", function()       {   toggleWindow(helpDialog);       });
-settingsButton.addEventListener("click", function() {   toggleWindow(settingsWindow);   });
-exitSettings.addEventListener("click", function()   {   toggleWindow(settingsWindow);   });
+function disableClearGuess() {
+  if (formInput.value.length === 0) {
+    clearButton.disabled = true;
+    guessButton.disabled = true;
+  } else {
+    clearButton.disabled = false;
+    guessButton.disabled = false;
+  }
+}
 
 function resetGame() {
-    formInput.value = "";
-    lastGuess.innerText = "?";
-    gameResult.innerText = results.default;
-    targetVal = generateTargetVal();
-    resetButton.disabled = true;
-    disableClearGuess();
-    adjustLevel(-currentLevel+1);
+  formInput.value = '';
+  lastGuess.innerText = '?';
+  gameResult.innerText = results.default;
+  targetVal = generateTargetVal();
+  resetButton.disabled = true;
+  disableClearGuess();
+  adjustLevel(-currentLevel + 1);
 }
 
-function adjustLevel(adjustLevel){
-    currentLevel += adjustLevel;
-    highVal += (adjustLevel*10);
-    highValField.value = highVal;
-    currentLevelViewer.innerText = currentLevel;
+
+function validValFields() {
+  const isValid = (parseInt(lowValField.value, 2) < parseInt(highValField.value, 2));
+  return isValid;
 }
 
-function levelUp(){
-    gameResult.innerHTML = results.correct;
-    adjustLevel(1);
-    targetVal = generateTargetVal();
+// Clear Button click Handler
+clearButton.addEventListener('click', () => {
+  formInput.value = '';
+  disableClearGuess();
+});
+
+// Reset Button Click Handler
+resetButton.addEventListener('click', () => {
+  resetGame();
+});
+
+// Clear, Guess Button state handler
+formInput.addEventListener('keyup', () => {
+  disableClearGuess();
+});
+
+// Toggles setting and help windows
+function toggleWindow(window) {
+  if (window.classList.contains('hide')) {
+    window.classList.remove('hide');
+  } else {
+    window.classList.add('hide');
+  }
 }
 
-//Disables clear and guess button when input is empty
-function disableClearGuess() {
-    if (formInput.value.length === 0) {
-        clearButton.disabled = true;
-        guessButton.disabled = true;
+// Help and Exit Button handlers
+helpButton.addEventListener('click', () => {
+  toggleWindow(helpDialog);
+});
+
+exitHelp.addEventListener('click', () => {
+  toggleWindow(helpDialog);
+});
+
+settingsButton.addEventListener('click', () => {
+  toggleWindow(settingsWindow);
+});
+
+exitSettings.addEventListener('click', () => {
+  toggleWindow(settingsWindow);
+});
+
+function clearValFieldsValidity() {
+  lowValField.setCustomValidity('');
+  highValField.setCustomValidity('');
+}
+
+// Handles the low value field in the settings dialog
+lowValField.addEventListener('keyup', () => {
+  if (validValFields()) {
+    clearValFieldsValidity();
+    if (lowVal !== parseInt(lowValField.value, 2)) {
+      lowVal = parseInt(lowValField.value, 2);
+      resetGame();
     }
-    else {
-        clearButton.disabled = false;
-        guessButton.disabled = false;
+  } else {
+    lowValField.setCustomValidity('Invalid Input');
+  }
+});
+
+highValField.addEventListener('keyup', () => {
+  if (validValFields()) {
+    clearValFieldsValidity();
+
+    if (highVal !== parseInt(highValField.value, 2)) {
+      highVal = parseInt(highValField.value, 2);
+      resetGame();
     }
-}
-
-//Checks if input data is a number; Updates input validity state if not;
-function validInput(userValue){
-    let isValid = (lowVal <= userValue && userValue <= highVal); 
-    
-    if (isValid) 
-        formInput.setCustomValidity("");  
-    else{   
-        formInput.setCustomValidity("Invalid Input");   
-    }
-
-   return isValid;
-}
-
-//Toggles setting and help windows
-function toggleWindow(window){
-
-    if(window.classList.contains("hide"))   {   window.classList.remove("hide");    }
-    else                                    {   window.classList.add("hide");       }
-
-}
-
-//Creates a random number between high and low value
-function generateTargetVal() {
-    return Math.round(Math.random() * (highVal - lowVal)) + lowVal;
-}
-
-//
-//Settings handlers
-//
-
-function validValFields(){
-    
-    let isValid = (parseInt(lowValField.value) < parseInt(highValField.value));
-    return isValid;
-}
-
-function clearValFieldsValidity(){
-    lowValField.setCustomValidity("");
-    highValField.setCustomValidity("");
-}
+  } else {
+    highValField.setCustomValidity('Invalid Input');
+  }
+});
